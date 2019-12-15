f6128310ddd3885250c690af4779df7c6a371c61
Julian Pormentilla <julian.pormentilla@gmail.com>
2019-12-15 19:05:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,8 +7,7 @@ end
 # Notes: position_taken? method 
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [
-  [0, 1, 2],
+WIN_COMBINATIONS = [ [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
